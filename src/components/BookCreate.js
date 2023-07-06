512c343a1a783a656610f6a900157dd614d90d02
Christian Johnson <charcoaldust@mac.com>
2023-07-06 14:46:33 -0700

    Added createBook form

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,11 +1,23 @@
-import React from "react";
+import { useState } from "react";
 
 const BookCreate = ({ onCreate }) => {
-  // Component Logic
+  const [title, setTitle] = useState("");
+  const handleChange = (event) => {
+    setTitle(event.target.value); //? Updates the title state.
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault(); //? Prevents the page from refreshing.
+    onCreate(title); //? Calls the function passed down from App.js
+    setTitle(""); //? Resets the input field.
+  };
   return (
     <div>
-      <h2>BookCreate reporting for duty</h2>
-      {/* Component Content */}
+      <h2>Add a book</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Title:</label>
+        <input type="text" value={title} onChange={handleChange} />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
