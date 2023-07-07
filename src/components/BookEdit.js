4f4345eb862469cb5a1cc4f9fd8f63d0243aefe7
Christian Johnson <charcoaldust@mac.com>
2023-07-06 21:36:03 -0700

    added BookEdit form and handlers

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { useState } from "react";
 
-const BookEdit = () => {
-  // Component Logic
+const BookEdit = ({ book, onSubmit }) => {
+  //*---State---*//
+  console.log(book.title);
+  const [title, setTitle] = useState(book.title);
+
+  //*---Event Handlers---*//
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(book.id, title);
+  };
   return (
-    <div>
-      <h2>BookEdit reporting for duty</h2>
-      {/* Component Content */}
-    </div>
+    <form className="book-edit" onSubmit={handleSubmit}>
+      <label htmlFor="">Title</label>
+      <input
+        type="text"
+        className="input"
+        onChange={handleChange}
+        value={title}
+      />
+      <button className="button is-primary">Save</button>
+    </form>
   );
 };
 
