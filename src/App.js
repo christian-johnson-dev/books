ea42605bddfefc1144922d2b7b107bad7545d6db
Christian Johnson <charcoaldust@mac.com>
2023-07-07 12:41:21 -0700

    Updated createBook to POST to db.json

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 const App = () => {
   const [books, setBooks] = useState([]); //* Not a book but an array of all the books.
 
-  const createBook = (title) => {
-    console.log("Need to add book with:", title);
-    const updatedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 1000000),
-        title: title,
-      },
-    ];
-    console.log(updatedBooks);
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title: title,
+    });
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
+
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
@@ -31,6 +28,7 @@ const App = () => {
     });
     setBooks(updatedBooks);
   };
+
   const deleteBookById = (id) => {
     console.log("Need to delete book with id:", id);
     const updatedBooks = books.filter((book) => book.id !== id); //
