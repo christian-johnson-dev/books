727b4b0a25d3860c1a14b4c22ef57424228b3376
Christian Johnson <charcoaldust@mac.com>
2023-07-10 12:01:20 -0700

    added custom provider to wrap useState

--- src/context/books.js
+++ src/context/books.js
@@ -1,5 +1,22 @@
 import { createContext } from "react";
+import { useState } from "react";
 
 const BooksContext = createContext();
+
+function Provider({ children }) {
+  const [count, setCount] = useState(0);
+
+  const valueToShare = {
+    count: count,
+    incrementCount: () => {
+      setCount(count + 1);
+    },
+  };
+  return (
+    <BooksContext.Provider value={valueToShare}>
+      {children}
+    </BooksContext.Provider>
+  );
+}
 
 export default BooksContext;
