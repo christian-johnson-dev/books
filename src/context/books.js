c19f757ecf7e38418a8f16c6c3a4d970be39457e
Christian Johnson <charcoaldust@mac.com>
2023-07-10 18:17:17 -0700

    main useContext container component

--- src/context/books.js
+++ src/context/books.js
@@ -1,16 +1,54 @@
-import { createContext } from "react";
-import { useState } from "react";
+import { createContext, useState } from "react";
+import axios from "axios";
 
 const BooksContext = createContext();
 
 function Provider({ children }) {
-  const [count, setCount] = useState(0);
+  const [books, setBooks] = useState([]); //* Not a book but an array of all the books.
+
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title: title,
+    });
+    const updatedBooks = [...books, response.data];
+    setBooks(updatedBooks);
+  };
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.patch(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+    console.log("Updated title: ", response.data.title);
+
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return {
+          ...book,
+          ...response.data,
+        };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+    const updatedBooks = books.filter((book) => book.id !== id); //
+    setBooks(updatedBooks);
+  };
 
   const valueToShare = {
-    count: count,
-    incrementCount: () => {
-      setCount(count + 1);
-    },
+    //* Note: these are identical key-value pairs so we can use the shorthand syntax. "books : books" is the same as "books"
+    books,
+    fetchBooks,
+    createBook,
+    editBookById,
+    deleteBookById,
   };
   return (
     <BooksContext.Provider value={valueToShare}>
@@ -18,5 +56,5 @@ function Provider({ children }) {
     </BooksContext.Provider>
   );
 }
-
-export default BooksContext;
+export { Provider }; // named export
+export default BooksContext; // default export
