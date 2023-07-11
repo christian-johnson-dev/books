1df90f59b8ef27802b80bfc95166600b466cc962
Christian Johnson <charcoaldust@mac.com>
2023-07-10 18:13:39 -0700

    moved handlers and state logic out of app.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
+import BooksContext from "./context/books";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
-import axios from "axios";
 
 const App = () => {
-  const [books, setBooks] = useState([]); //* Not a book but an array of all the books.
-
-  const fetchBooks = async () => {
-    const response = await axios.get("http://localhost:3001/books");
-    setBooks(response.data);
-  };
+  const { fetchBooks } = useContext(BooksContext);
 
   useEffect(() => {
     fetchBooks();
   }, []);
 
-  const createBook = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", {
-      title: title,
-    });
-    const updatedBooks = [...books, response.data];
-    setBooks(updatedBooks);
-  };
-
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.patch(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
-    console.log("Updated title: ", response.data.title);
-
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return {
-          ...book,
-          ...response.data,
-        };
-      }
-      return book;
-    });
-    setBooks(updatedBooks);
-  };
-
-  const deleteBookById = async (id) => {
-    await axios.delete(`http://localhost:3001/books/${id}`);
-    const updatedBooks = books.filter((book) => book.id !== id); //
-    setBooks(updatedBooks);
-  };
   return (
     <div className="App">
       <h1>Reading List</h1>
-      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
-      <BookCreate onCreate={createBook} />
+      <BookList />
+      <BookCreate />
     </div>
   );
 };
